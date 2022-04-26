141053d5633645be266f848257e55195eed20113
V-Gira <virgile.girardin@gmail.com>
2022-04-27 00:53:56 +0200

    set up pagination

--- src/components/ContentArea.tsx
+++ src/components/ContentArea.tsx
@@ -1,7 +1,10 @@
-import { Grid, Box, TextField } from "@mui/material";
+import { Grid, Box, TextField, CircularProgress, Stack, Pagination } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { PackageCard } from "./PackageCard";
 import axios from "axios";
+import usePagination from "../hooks/usePagination";
+const { v4: uuidv4 } = require('uuid');
+
 
 export const ContentArea: React.FC<{}> = (props) => {
 
@@ -9,16 +12,21 @@ export const ContentArea: React.FC<{}> = (props) => {
         search: "",
         query: "",
     });
-    const [data, setData] = useState<any>();
+    const [data, setData] = useState<any>([]);
     const [error, setError] = useState<any>();
     const [loading, setLoading] = useState<boolean>(false);
-    const KEY = process.env.REACT_APP_API_KEY
+    const [page, setPage] = React.useState(1)
+    // const KEY = process.env.REACT_APP_API_KEY
+
+    const _DATA = usePagination(data, 5)
     
     const queryAPI = async (query: string) => {
         console.log("hi")
+        // const url = `https://libraries.io/api/search?q=${query}&sort=stars&platforms=bower&per_page=100&api_key=${KEY}`
+        const url = `https://libraries.io/api/bower-search?q=${query}&sort=stars&per_page=100`
         setLoading(true)
         try{
-            const response = await axios.get(`https://libraries.io/api/search?q=${query}&sort=stars&api_key=${KEY}`)
+            const response = await axios.get(url)
             setData(response.data);
             setError(null);
         } catch (err: any) {
@@ -32,14 +40,23 @@ export const ContentArea: React.FC<{}> = (props) => {
     const handleSubmit = (event:React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         queryAPI(input.query)
-        console.log(data)
-        
-        
+        console.log(data.length)
     }
     
     const handleChange = (prop: string) => (event: React.ChangeEvent<HTMLInputElement>) => {
         setInput({ ...input, [prop]: event.target.value });
     };
+
+    const getOwner = (url: string) => {
+        let owner = url.substring(19)
+        owner = owner.substring(0, owner.indexOf("/"))
+        return owner
+    }
+
+    const handleChangePage = (event:any, newPage:any) => {
+        setPage(newPage);
+        _DATA.jump(newPage)
+      };
 
     useEffect(() => {
         queryAPI(input.query)
@@ -50,10 +67,28 @@ export const ContentArea: React.FC<{}> = (props) => {
             <form  onSubmit={handleSubmit}>
                 <TextField fullWidth label="Search..." value={input.query} onChange={handleChange("query")} />
             </form>
-            <Grid container spacing={7} mt={0}>
-                    {data? data.map((item: any) => (
-                        <PackageCard description={item.description} user={item.name} stars={item.stars} key={item.latest_release_published_at} />
-                    )) : "nothing"}
+            <Grid container spacing={3} mt={0}>
+                <Grid container item xs={12} justifyContent={"center"}>
+                    <Stack>
+                        <Pagination 
+                            showFirstButton 
+                            showLastButton 
+                            shape="rounded" 
+                            variant="outlined" 
+                            count={Math.ceil(data.length / 5)} 
+                            page={page}
+                            onChange={handleChangePage}
+                            /> 
+                    </Stack>
+                </Grid>
+                    {(data.length !== 0)? _DATA.currentData().map((item: any) => (
+                        <PackageCard name={item.name} homepage={item.homepage} description={item.description} owner={getOwner(item.repository_url)} stars={item.stars} key={uuidv4()} />
+                    )) : 
+                    <Grid container item xs={12} justifyContent={"center"}>
+                        <CircularProgress />
+                    </Grid>
+                    }
+
             </Grid>
         </Box>
     )
--- src/components/PackageCard.tsx
+++ src/components/PackageCard.tsx
@@ -1,26 +1,30 @@
-import { Grid, Typography } from "@mui/material";
+import { Grid, Paper, Typography } from "@mui/material";
 import React, { useState } from "react";
 
 export const PackageCard: React.FC<{
-   // name: string;
+   name: string;
     description: string;
-  //  homepage: string;
-    user: string;
+   homepage: string;
+    owner: string;
     stars: number;
 }> = (props) => {
     return (
         <Grid item xs={12}>
-            <Grid container>
+            <Paper>
+            <Grid container p={1}>
                 <Grid item xs={8}>
+                    <Typography variant="body1">{props.name}</Typography>
+                    <Typography variant="body1">{props.homepage}</Typography>
                     <Typography variant="body1">{props.description}</Typography>
                 </Grid>
-                <Grid item xs={2}>
-                    <Typography variant="body1">{props.user}</Typography>
+                <Grid item xs={3}>
+                    <Typography variant="body1">{props.owner}</Typography>
                 </Grid>
-                <Grid item xs={2}>
+                <Grid item xs={1}>
                     <Typography variant="body1">{props.stars}</Typography>
                 </Grid>
             </Grid>
+            </Paper>
         </Grid>
         
     )
